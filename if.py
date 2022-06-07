20aa8b7d47619efedc590c337ce273565a594b43
“hossain01785” <hossain01785@gmail>
2022-06-07 21:51:49 +0600

    if.py

---
+++ if.py
@@ -0,0 +1,12 @@
+a=int (input("Enter A First Number:"))
+b=int (input("Enter A Another Number:"))
+c=input("Enter A Officer:")
+if c=="+":
+    print("the nmbr is right:",a+b)
+elif c=="-":
+    print("the nmbr is worng:",a-b)
+elif  c=="*":
+    print("the nmbrr is good:",a*b)
+elif c=="/":
+    print("the nmbr is bad:",a/b)
+
